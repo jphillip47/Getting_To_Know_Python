709a8d9c920d8cc88e3ed1b965f8af251977a2da
JackiePhillips <>
2021-07-26 17:44:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen 
+
+t: Turtle = Turtle() 
+s: Screen = Screen() 
+
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+
+
+
+
